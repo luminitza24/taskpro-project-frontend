acaf2628d9e4811b0a3eed4447d0f0e78ded0e3b
Radu-Gabriel <110191915+gabrielRaduDanila@users.noreply.github.com>
2023-12-01 20:45:46 +0200

    last

--- src/components/screens-page/ScreensPage.js
+++ src/components/screens-page/ScreensPage.js
@@ -1,14 +1,12 @@
 import HeaderDashboard from './HeaderDashboard';
 import MainDashboard from './MainDashboard';
 import './screens-page.css';
-import EditProfileModal from '../header/EditProfileModal';
 
 const ScreensPage = () => {
   return (
     <div className='screens-page bg-secondary text-center'>
       <HeaderDashboard />
       <MainDashboard />
-      <EditProfileModal />
     </div>
   );
 };
--- src/components/shared-layout/SharedLayout.js
+++ src/components/shared-layout/SharedLayout.js
@@ -2,12 +2,12 @@ import './SharedLayout.css';
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
 import Loading from '../loading/Loading';
-import Navbar from '../header/Header';
+// import Navbar from '../header/Header';
 
 const SharedLayout = () => {
   return (
     <div className='container'>
-      <Navbar />
+      {/* <Navbar /> */}
       <Suspense fallback={<Loading />}>
         <Outlet />
       </Suspense>
