c007127e7ac8c5ea7681e79b6e915d59d7220500
CristinaAI22 <cristina.ionica22@gmail.com>
2023-12-04 23:01:00 +0200

    11

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,13 +3,16 @@ import React from "react";
 import ThemeSwitcher from "./ThemeSwitcher";
 import UserInfo from "./UserInfo";
 import { useSelector } from "react-redux";
+import { useTheme } from "./ThemeContext";
 
 const Header = () => {
   const state = useSelector((state) => state.auth);
   // console.log(state.user);
+  const theme = useTheme();
   return (
     <>
       <div className="header">
+        <p style={{ color: theme.palette.primary.info }}>ceva </p>
         <ul
           className=" d-flex flex-row justify-content-end navbar-nav  "
           style={{
