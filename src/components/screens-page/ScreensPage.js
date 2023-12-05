16625e9ae57487ebc2a03ad1d0597764c08e62e5
Radu-Gabriel <110191915+gabrielRaduDanila@users.noreply.github.com>
2023-12-05 22:43:02 +0200

    update

--- src/components/screens-page/ScreensPage.js
+++ src/components/screens-page/ScreensPage.js
@@ -4,15 +4,21 @@ import MainDashboard from './MainDashboard';
 import './screens-page.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { getBoardData } from '../../features/board-slice/operations';
+import { selectBoardData } from '../../features/board-slice/selectors';
+import Loading from '../loading/Loading';
 
 const ScreensPage = () => {
   const dispatch = useDispatch();
   const boardId = '656639d36713cb888590d983';
+  const boadData = useSelector(selectBoardData);
 
   useEffect(() => {
     dispatch(getBoardData({ _id: boardId }));
   }, [dispatch]);
 
+  if (!boadData) {
+    return <Loading />;
+  }
   // const state = useSelector((state) => state.auth);
   // console.log(state);
   return (
--- src/features/board-slice/boardSlice.js
+++ src/features/board-slice/boardSlice.js
@@ -11,65 +11,7 @@ import {
 } from './operations';
 
 const initialState = {
-  bordData: {
-    _id: '656639d36713cb888590d983',
-    title: 'new title',
-    __v: 0,
-    lists: [
-      {
-        _id: '65663be08e3fb6d2d8e842ca',
-        title: 'new list1',
-        owner: '656639d36713cb888590d983',
-        __v: 0,
-        cards: [
-          {
-            _id: '65679150ecdc70e9ab2e5575',
-            title: 'card title',
-            description: 'some card description',
-            labelColor: '#8FA1D0',
-            deadline: '02/12/2023',
-            owner: '65663be08e3fb6d2d8e842ca',
-            __v: 0,
-          },
-          {
-            _id: '65679155ecdc70e9ab2e5578',
-            title: 'card 2 title',
-            description: 'some card description',
-            labelColor: '#BEDBB0',
-            deadline: '06/12/2023',
-            owner: '65663be08e3fb6d2d8e842ca',
-            __v: 0,
-          },
-        ],
-      },
-      {
-        _id: '656791a9ecdc70e9ab2e557b',
-        title: 'new list2',
-        owner: '656639d36713cb888590d983',
-        __v: 0,
-        cards: [
-          {
-            _id: '656791b8ecdc70e9ab2e557e',
-            title: 'card 2 title',
-            description: 'some card description',
-            labelColor: '#FFFFFF',
-            deadline: '06/12/2023',
-            owner: '656791a9ecdc70e9ab2e557b',
-            __v: 0,
-          },
-          {
-            _id: '656791beecdc70e9ab2e5581',
-            title: 'card 1 title',
-            description: 'some card description',
-            labelColor: '#FFFFFF',
-            deadline: '06/12/2023',
-            owner: '656791a9ecdc70e9ab2e557b',
-            __v: 0,
-          },
-        ],
-      },
-    ],
-  },
+  bordData: null,
   isLoading: false,
   isError: false,
   filter: null,
